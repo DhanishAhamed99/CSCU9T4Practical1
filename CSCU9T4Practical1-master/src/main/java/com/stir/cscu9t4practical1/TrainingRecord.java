c4cc9ac904cfa9c4569ccfbb338058bdf29e0b50
DhanishAhamed99 <123808663+DhanishAhamed99@users.noreply.github.com>
2023-03-31 19:34:07 +0400

    CSCU9T4Practical1- first commit

---
+++ CSCU9T4Practical1-master/src/main/java/com/stir/cscu9t4practical1/TrainingRecord.java
@@ -0,0 +1,69 @@
+// An implementation of a Training Record as an ArrayList
+package com.stir.cscu9t4practical1;
+
+
+
+
+import java.util.*;
+
+
+public class TrainingRecord {
+    private List<Entry> tr;
+    
+    public TrainingRecord() {
+        tr = new ArrayList<Entry>();
+    } //constructor
+    
+    // add a record to the list
+   public String addEntry(Entry e){
+       ListIterator<Entry> iter = tr.listIterator();
+       String result = "Record added \n";
+       while (iter.hasNext()) {
+          Entry current = iter.next();
+          if (e.getName().equalsIgnoreCase(current.getName()) && e.getMonth()== current.getMonth()&& e.getDay()== current.getDay() && e.getYear()== current.getYear()){
+              return("Duplicate entry entered"); 
+            }     
+       }
+       tr.add(e);
+       return result;
+   } // addClass
+   
+   // look up the entry of a given day and month
+   public String lookupEntry (int d, int m, int y) {
+       ListIterator<Entry> iter = tr.listIterator();
+       String result = "No entries found";
+       while (iter.hasNext()) {
+          Entry current = iter.next();
+          if (current.getDay()==d && current.getMonth()==m && current.getYear()==y) 
+             result = current.getEntry();
+            }
+       return result;
+   } // lookupEntry
+   
+      public String lookupAllEntry (int d, int m, int y) {
+       ListIterator<Entry> iter = tr.listIterator();
+       String result = "No entries found";
+       String currResult;
+       String finalResult = "";
+       while (iter.hasNext()) {
+          Entry current = iter.next();
+          if (current.getDay()==d && current.getMonth()==m && current.getYear()==y){ 
+             currResult = current.getEntry();
+             finalResult = finalResult.concat(currResult);
+             result = finalResult;
+          }
+            }
+       return result;
+       
+   } // lookupAllEntry
+   
+   // Count the number of entries
+   public int getNumberOfEntries(){
+       return tr.size();
+   }
+   // Clear all entries
+   public void clearAllEntries(){
+       tr.clear();
+   }
+   
+} // TrainingRecord
